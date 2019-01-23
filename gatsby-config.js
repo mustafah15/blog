3b074582545facf7389e1e90f811655172a9ba63
Mustafa Hussain <mustafa.hussain93@gmail.com>
2019-01-23 21:45:58 +0200

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,8 +59,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Mustafa Hussain's blog`,
+        short_name: `Mustafah15`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
