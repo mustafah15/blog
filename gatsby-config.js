66193cf0ed6ed6282aa7208ed31aa6da1dfea286
mustafah15 <mustafa.hussain93@gmail.com>
2019-02-20 12:07:23 +0200

    add google tracking-id

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-48540732-2`,
       },
     },
     `gatsby-plugin-feed`,
